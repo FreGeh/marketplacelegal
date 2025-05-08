1a123698c26dafee0db8e009b699ba378570218a
fregeh <fregeh7@gmail.com>
2025-05-08 22:46:24 +0200

    clerk but

--- components/navigation/MainNav.tsx
+++ components/navigation/MainNav.tsx
@@ -52,6 +52,7 @@ export default function MainNav({
                 <SignedOut>
                     <SignInButton mode="modal">
                         <button className="rounded-md bg-primary px-4 py-2 text-lg text-black shadow hover:bg-primary/90">
+                            Sign in
                         </button>
                     </SignInButton>
                 </SignedOut>
