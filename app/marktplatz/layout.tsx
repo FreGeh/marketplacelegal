305f8dd1ced77f580ae485d2e553028a522cbfa8
fregeh <fregeh7@gmail.com>
2025-05-10 15:09:25 +0200

    stuff

--- app/marktplatz/layout.tsx
+++ app/marktplatz/layout.tsx
@@ -6,7 +6,6 @@ export default function SolutionsLayout({
 }) {
   return (
     <div className="container mx-auto p-6">
-      <h1 className="mb-6 text-center text-4xl font-bold">Unsere Lösungen</h1>
       {children}
     </div>
   );
